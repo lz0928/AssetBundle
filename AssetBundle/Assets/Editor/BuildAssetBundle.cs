caf7d3a7edba603a778c506ca1ce0161229e6f39
lz0928 <1137556826@qq.com>
2018-08-02 09:38:00 +0800

    创建AssetBundleProject

---
+++ AssetBundle/Assets/Editor/BuildAssetBundle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class BuildAssetBundle {
+
+    [MenuItem("AssetBundleTools/BuildAssetBundles")]
+	public static void BuildBundle()
+    {
+        //打包AB输出路径
+        string strABOutPathDir = string.Empty;
+        //获取“StreamingAsset”数值
+        strABOutPathDir = Application.streamingAssetsPath;
+
+        if (!Directory.Exists(strABOutPathDir))
+        {
+            Directory.CreateDirectory(strABOutPathDir);
+        }
+        BuildPipeline.BuildAssetBundles(strABOutPathDir, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+    }
+}
